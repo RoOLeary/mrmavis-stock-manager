ba4c4ac522b77e46edc14526af2c5b0ffa219cf9
Ronan <roleary81@gmail.com>
2024-10-14 13:16:43 +0200

    shoot this to live

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -1,13 +1,26 @@
+import { useState, useEffect } from "react";
+import PurchaseProduct from "../components/PurchaseProduct";
+
 const Order = () => {
+  const [orderNumber, setOrderNumber] = useState("");
 
+  // Generate a random order number when the component mounts
+  useEffect(() => {
+    const randomOrderNumber = Math.floor(Math.random() * 1000000); // Generate random order number
+    // @ts-ignore
+    setOrderNumber(randomOrderNumber);
+  }, []);
 
-    return (
+  return (
+    <div className="container mx-auto py-8">
+      
+        <div className="information w-1/2 mx-auto p-8 text-gray text-normal">
+            <p>Place your order</p>
+        </div>
+        <PurchaseProduct />
+      
+    </div>
+  );
+};
 
-        <div>Order</div>
-
-    )
-
-
-}
-
-export default Order; 
+export default Order;
